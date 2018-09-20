0b077bdc4f0116d7514bef1b46be0e79d862bfc3
zhangliming <13510929584@163.com>
2018-09-20 15:07:19 +0800

    first

---
+++ junit-test/src/main/java/com/zlm/junittest/JunitTestApplication.java
@@ -0,0 +1,12 @@
+package com.zlm.junittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestApplication.class, args);
+	}
+}
